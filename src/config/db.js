e3dad56d1c0dc4f1f3c22acd2a5d16b86214db0d
Hussain Lodhi <hasanlodhi000@gmail.com>
2026-06-10 08:32:39 +0500

    updated db connection for neon

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,11 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
-pool.on('connect', () => {
-  console.log('PostgreSQL Database se connection pakka ho gaya! 🎉');
-});
-
-export default pool;
+module.exports = pool;
